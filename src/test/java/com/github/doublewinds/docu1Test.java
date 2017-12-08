f2b6d3c3be0161cba8e582f4799ce8055e34a536
doublewinds <wind860822@gmail.com>
2017-12-08 20:33:30 +0800

    update badge

--- src/test/java/com/github/doublewinds/docu1Test.java
+++ src/test/java/com/github/doublewinds/docu1Test.java
@@ -16,5 +16,35 @@ public class docu1Test {
         // Assert
         Assert.assertEquals("不是三角形", result);
     }
+    public void getType1() throws Exception {
+        // Arrange
+        docu1 triangle = new docu1(3, 4, 5);
+
+        // Act
+        String result = triangle.getType();
+
+        // Assert
+        Assert.assertEquals("直角三角形", result);
+    }
+    public void getType2() throws Exception {
+        // Arrange
+        docu1 triangle = new docu1(3, 4, 4);
+
+        // Act
+        String result = triangle.getType();
+
+        // Assert
+        Assert.assertEquals("銳角三角形", result);
+    }
+    public void getType3() throws Exception {
+        // Arrange
+        docu1 triangle = new docu1(7, 4, 4);
+
+        // Act
+        String result = triangle.getType();
+
+        // Assert
+        Assert.assertEquals("鈍角三角形", result);
+    }
 }
 
