4319332c32d71bb820eda6c9b8d387ba9232b533
doublewinds <wind860822@gmail.com>
2018-01-18 22:40:12 +0800

    Add files via upload

--- src/test/java/Phontest.java
+++ src/test/java/Phontest.java
@@ -9,14 +9,44 @@ public class Phontest
 	int timeresult []=new int[150];
 	int gapresult []=new int[150];
 	int test=50,resultt=1,resultg=1;
-	@Test
-	public void test() throws Exception
+	public void  value()
 	{
 		for(int times=0;times<150;times++)
 		{
 			decible[times]=test;
 			test++;
+			if(times<20)
+			{
+				timeresult[times]=0;
+				gapresult[times]=resultg;
+				resultg++;
+				if(resultg==3)
+				{
+					resultg=0;
+				}
+			}
+			else if(times>19&&times<139)
+			{
+				timeresult[times]=resultt;
+				resultt++;
+				if(times==138)
+				{
+					resultt=0;
+				}
+				gapresult[times]=0;
+			}
+			else
+			{
+				timeresult[times]=resultt;
+				resultt++;
+				gapresult[times]=0;
+			}
 		}	
+	}
+	@Test
+	public void test() throws Exception
+	{
+		value();
 		Phone ph = new Phone();
 		for(int times=0;times<150;times++)
 		{
@@ -24,6 +54,18 @@ public class Phontest
 			System.out.println(ph.time+"," +ph.gap);
 		}
 		Assert.assertEquals(10, ph.time);
+	}
+	@Test
+	public void test2() throws Exception
+	{	
+		value();
+		Phone ph = new Phone();
+		for(int times=0;times<150;times++)
+		{
+			ph.rulejudge(decible[times]);
+			System.out.println(ph.time+"," +ph.gap);
+		}
 		Assert.assertEquals(0, ph.gap);
 	}
+
 }
