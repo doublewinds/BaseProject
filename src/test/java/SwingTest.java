5e96fbcdd55c9b25ab27d69a26af9c7adccc0c95
jemmy1597 <32162713+jemmy1597@users.noreply.github.com>
2018-01-18 18:03:44 +0800

    Add files via upload

--- src/test/java/SwingTest.java
+++ src/test/java/SwingTest.java
@@ -4,10 +4,13 @@ import org.junit.Test;
 public class SwingTest {
 
 	@Test
-	public void testTimeSet() throws Exception {
+	public void testTimeSet1() throws Exception {
 		Swing sw = new Swing();
 		int temp = sw.getcustom_time();
 		sw.timeSet("100");
+		Assert.assertEquals(temp , sw.getcustom_time());
+		
+		sw.timeSet("-10");
 		Assert.assertEquals(temp , sw.getcustom_time());
 		
 		sw.timeSet("abc");
@@ -16,7 +19,7 @@ public class SwingTest {
 		sw.timeSet("5");
 		Assert.assertEquals(5, sw.getcustom_time());
 	}
-
+	
 	@Test
 	public void testDbSet() throws Exception {
 		Swing sw = new Swing();
@@ -29,5 +32,13 @@ public class SwingTest {
 		
 		sw.dbSet("60");
 		Assert.assertEquals(60, sw.getcustom_db());
+	}
+	
+	@Test
+	public void testshowStatus() throws Exception {
+		Swing sw = new Swing();
+		sw.showStatus("Test\nHello world");
+		String s = sw.textarea.getText();
+		Assert.assertEquals("Test\nHello world", s);
 	}
 }
