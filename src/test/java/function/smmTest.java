fd92aedc99818f9019cdbf7fa2732e64fd25dfe4
NazcaLiu <32123596+NazcaLiu@users.noreply.github.com>
2018-01-19 15:04:12 +0800

    TC

---
+++ src/test/java/function/smmTest.java
@@ -0,0 +1,19 @@
+package function;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class smmTest {
+	
+	@Test
+	public void test() throws Exception{
+		SoundMeterMonitor smm = new SoundMeterMonitor();
+		smm.startMonitor();
+		int[] testQueue = smm.getDataQueue();
+		int testKey = smm.getKey();
+		Assert.assertEquals(testQueue[testKey],smm.getData());
+		Assert.assertEquals(true,smm.soundMeterIsActive());
+		System.out.println(smm.getSoundWave());
+		smm.closeMonitor();
+	}
+
+}
